aa66971270c8d2adec31ae0cdbad32e366ac9edd
Sergey Sedov <33500911+sergeeximius@users.noreply.github.com>
2020-11-24 22:23:29 +0400

    Update veeam-job.py

--- lib/veeam-job.py
+++ lib/veeam-job.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2020, Sergey Sedov <serge.eximius@gmail.com>
-# MIT https://github.com/sergeeximius/ansible-veeam/blob/main/LICENSE
+# GNU General Public License v3.0+ (see https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
